6e525ec0c44e5b4ed874b22aadd611f3039d0da2
Yuvraj Singh <yuvrajraghuvanshisingh@gmail.com>
2025-07-12 17:08:12 +0530

    Episode 3 ofnamaste-react course

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child1", key: "child1" }, [
-        React.createElement("h1", { key: "h1-child1" }, "H1 in child1"),
-        React.createElement("h2", { key: "h2-child1" }, "H2 in child1"),
-    ]),
-    React.createElement("div", { id: "child2", key: "child2" }, [
-        React.createElement("h1", { key: "h1-child2" }, "H1 in child2"),
-        React.createElement("h2", { key: "h2-child2" }, "H2 in child2"),
-    ]),
-]);
+import ReactDOM from "react-dom/client";
 
+const Title = () => (
+    <h1 className="head" tabIndex="5">
+        Namaste React Using JSX
+    </h1>
+);
 
-console.log(parent);
+const HeadingComponent = () => (
+    <div id="container">
+        <Title/>
+        <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HeadingComponent/>);
