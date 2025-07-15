3b7882af4c4859f63dd7045a85594a11c91b727f
Yuvraj Singh <yuvrajraghuvanshisingh@gmail.com>
2025-07-15 23:38:56 +0530

    Update file of Namaste-React coirse episode-4

--- App.js
+++ App.js
@@ -1,19 +1,276 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-    <h1 className="head" tabIndex="5">
-        Namaste React Using JSX
-    </h1>
-);
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
+            </div>
+            <div className="nav-items">
+             <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Contact Me</li>
+                <li>Cart</li>
+                <li></li>
+             </ul>
+            </div>
+        </div>
+    );
+};
 
-const HeadingComponent = () => (
-    <div id="container">
-        <Title/>
-        <h1 className="heading">Namaste React Functional Component</h1>
-    </div>
-);
+const RestaurantCard = (props) => {
+    const {resData} = props;
+    return (
+        <div className="res-card">
+            <img
+            className="res-logo"
+            alt="res-logo" 
+            src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_292,h_300/" + resData.info.cloudinaryImageId} 
+            /> 
+            <h3>{resData.info.name}</h3>
+            <h4>{resData.info.cuisines.join(',')}</h4>
+            <h4>{resData.info.avgRatingString}</h4>
+            <h4>{resData.info.costForTwo}</h4>
+            
+        </div>
+    );
+};
+
+
+const resList = [
+  {
+    info: {
+      id: "435680",
+      name: "Pizza Hut",
+      cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/9/2393b7fd-0afe-4703-8774-3e1e97b4c3b4_435680.JPG",
+      locality: "Lallapura",
+      areaName: "Chetganj",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "8.3K+",
+      sla: {
+        deliveryTime: 31,
+        lastMileTravel: 2.9,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "2.9 km",
+        iconType: "ICON_TYPE_EMPTY"
+      },
+      availability: {
+        nextCloseTime: "2025-07-16 04:00:00",
+        opened: true
+      },
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {}
+        }
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {}
+        }
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--"
+        }
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    analytics: {
+      context: "seo-data-1326e3df-2643-4aef-b392-1d39ca2ada80"
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/varanasi/pizza-hut-lallapura-chetganj-rest435680",
+      type: "WEBLINK"
+    }
+  },
+  {
+    info: {
+      id: "87263",
+      name: "KFC",
+      cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/7f6d542d-1290-4934-95d3-a7135b77b661_87263.JPG",
+      locality: "Celestial Building",
+      areaName: "Sigra",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Fast Food", "Rolls & Wraps"],
+      avgRating: 4.3,
+      parentId: "547",
+      avgRatingString: "4.3",
+      totalRatingsString: "19K+",
+      sla: {
+        deliveryTime: 30,
+        lastMileTravel: 3,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "3.0 km",
+        iconType: "ICON_TYPE_EMPTY"
+      },
+      availability: {
+        nextCloseTime: "2025-07-16 05:00:00",
+        opened: true
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Burger.png",
+            description: "Delivery!"
+          }
+        ]
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Burger.png"
+                }
+              }
+            ]
+          },
+          textBased: {},
+          textExtendedBadges: {}
+        }
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹59"
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {}
+        }
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--"
+        }
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    analytics: {
+      context: "seo-data-1326e3df-2643-4aef-b392-1d39ca2ada80"
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/varanasi/kfc-celestial-building-sigra-rest87263",
+      type: "WEBLINK"
+    }
+  },
+  {
+    info: {
+      id: "433786",
+      name: "The Good Bowl",
+      cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/15/ad05bb22-2595-4934-964c-c61b89c43665_433786.jpg",
+      locality: "Raja Moti Chand Road",
+      areaName: "Mahmoorganj",
+      costForTwo: "₹400 for two",
+      cuisines: ["Biryani", "North Indian", "Pastas", "Punjabi", "Desserts", "Beverages"],
+      avgRating: 4.4,
+      parentId: "7918",
+      avgRatingString: "4.4",
+      totalRatingsString: "2.1K+",
+      sla: {
+        deliveryTime: 48,
+        lastMileTravel: 4.1,
+        serviceability: "SERVICEABLE",
+        slaString: "45-50 mins",
+        lastMileTravelString: "4.1 km",
+        iconType: "ICON_TYPE_EMPTY"
+      },
+      availability: {
+        nextCloseTime: "2025-07-15 23:59:00",
+        opened: true
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {}
+        }
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹99"
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {}
+        }
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--"
+        }
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    analytics: {
+      context: "seo-data-1326e3df-2643-4aef-b392-1d39ca2ada80"
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/varanasi/the-good-bowl-raja-moti-chand-road-mahmoorganj-rest433786",
+      type: "WEBLINK"
+    }
+  }
+]
+
+
+
+const Body =() => {
+    return(
+        <div className="body">
+            <div className="search"> Search </div>
+                <div className="res-container">
+                   <RestaurantCard resData={resList[0]} />
+                   <RestaurantCard resData={resList[1]} />
+                   <RestaurantCard resData={resList[2]} />
+            </div>
+        </div>
+    );
+};
+
+const AppLayout =() => {
+    return (
+        <div className="app">
+             <Header/> 
+             <Body/>
+        </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent/>);
+root.render(<AppLayout/>);
