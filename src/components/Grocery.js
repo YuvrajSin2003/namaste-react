a03d0335163cd40ce11f73f1f5d17265b469cf9c
YuvrajSin2003 <yuvrajraghuvanshisingh@gmail.com>
2025-07-31 15:41:52 +0530

    Completed epiosde 9

---
+++ src/components/Grocery.js
@@ -0,0 +1,4 @@
+const Grocery = () => {
+    return <h1>Our online grocery store , And we have a lot of child stores</h1>
+}
+export default Grocery;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,31 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/contants";
 import { Link } from "react-router-dom";
+import useOnlineState from "../utils/useOnlineState";
 
 
  const Header = () => {
 
     const [btnNameReact , setbtnNameReact] = useState("Login")
+
+    const onLineStatus = useOnlineState();
     return(
         <div className="header">
             <div className="logo-container">
                 <img className="logo" src={LOGO_URL}/>
             </div>
             <div className="nav-items">
+                <ul>
+                    <li>
+                        online State : {onLineStatus ? "✅" : "❌" }
+                    </li>
+                </ul>
              <ul>
                 <li> <Link to="/">Home </Link></li>
                 <li> <Link to="/about">About </Link></li>
                 <li> <Link to="/contact">Contact </Link></li>
                 <li> <Link to="/cart">Cart </Link></li>
+                <li> <Link to="/grocery">Grocery </Link></li>
                 <button className="LogIn" onClick={() => {
                     btnNameReact === "LogIn" ? setbtnNameReact("LogOut") : setbtnNameReact("LogIn");
 
